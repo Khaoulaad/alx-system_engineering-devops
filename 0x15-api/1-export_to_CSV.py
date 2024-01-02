b2d13bb88f48ce58f48e1b7e771d2fc73703d9c4
Khaoulaad <ouajaa.khaoula.com>
2024-01-02 08:31:05 -0800

    0

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,17 +1,31 @@
 #!/usr/bin/python3
-"""Script to export data in the CSV format"""
+"""
+Module that returns information for a given employee ID.
+
+About his/her TODO list progress from a REST API.
+"""
+
 import csv
-import requests as r
-import sys
+from sys import argv
+import requests
+
+
+def fetch_data(id):
+    req = requests.get("https://jsonplaceholder.typicode.com/users/{}"
+                       .format(id))
+    user = req.json()
+
+    req = requests.get("https://jsonplaceholder.typicode.com/users/{}/todos"
+                       .format(id))
+    todos = req.json()
+
+    file_name = "{}.csv".format(user["id"])
+    with open(file_name, "w") as fi:
+        writer = csv.writer(fi, quoting=csv.QUOTE_ALL)
+        for todo in todos:
+            writer.writerow([user["id"], user["username"],
+                            todo["completed"], todo["title"]])
+
 
 if __name__ == "__main__":
-    user_id = sys.argv[1]
-    url = "https://jsonplaceholder.typicode.com/"
-    usr = r.get(url + "users/{}".format(user_id)).json()
-    username = usr.get("username")
-    to_do = r.get(url + "todos", params={"userId": user_id}).json()
-
-    with open("{}.csv".format(user_id), "w", newline="") as csvfile:
-        writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
-        [writer.writerow([user_id, username, elm.get("completed"),
-                          elm.get("title")]) for elm in to_do]
+    fetch_data(argv[1])
